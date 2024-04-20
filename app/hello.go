6e4f9f0540de926e7ee488ed5893cf94eb615870
popoff <v.popoff@gmail.com>
2024-04-20 16:22:37 +0300

    added test web server

--- app/hello.go
+++ app/hello.go
@@ -3,20 +3,24 @@ package main
 import "fmt"
 import "github.com/jessevdk/go-flags"
 import "github.com/p0poff/mock/app/storage"
+import "github.com/p0poff/mock/app/server"
 import "os"
 import "log"
 
-var opts struct {
+type options struct {
 	Verbose []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
 	FileDb  string `short:"f" long:"filedb" description:"file db path" required:"true"`
 
 	Logger struct {
 		FileName string `long:"file" env:"FILE_LOG"  default:"../data/hello.log" description:"location of spam log"`
 	}
+
+	Port string `short:"p" long:"port" env:"PORT" default:"8080" description:"port to listen on"`
 }
 
 func main() {
 	fmt.Println("Hello, World!")
+	var opts options
 
 	p := flags.NewParser(&opts, flags.Default)
 	_, err := p.Parse()
@@ -55,10 +59,16 @@ func main() {
 		return
 	}
 
-	err = db.AddProduct("Apple", 0.5)
-	if err != nil {
-		fmt.Println("Error adding product:", err)
-		return
+	// err = db.AddProduct("Apple", 0.5)
+	// if err != nil {
+	// 	fmt.Println("Error adding product:", err)
+	// 	return
+	// }
+
+	s := server.NewServer(opts.Port, db)
+
+	if err = s.Start(); err != nil {
+		log.Printf("[ERROR] %v", err)
 	}
 
 	fmt.Printf("Verbosity: %v\n", opts.Verbose)
---
+++ app/server/server.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"encoding/json"
+	"github.com/p0poff/mock/app/storage"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	port string
+	db   *storage.SQLiteDB
+}
+
+func NewServer(port string, db *storage.SQLiteDB) *Server {
+	s := &Server{
+		port: port,
+		db:   db,
+	}
+
+	return s
+}
+
+func (s *Server) helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Add your hello logic here
+	_ = s.db.AddProduct("Apple", 11)
+	w.Write([]byte("Hello, World!"))
+}
+
+func (s *Server) defaultHandler(w http.ResponseWriter, r *http.Request) {
+	uri := r.URL.Path
+
+	// Return JSON response
+	response := map[string]string{"uri": uri}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		log.Println("[ERROR] Failed to marshal JSON response:", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+func (s *Server) Start() error {
+	addr := ":" + s.port
+	log.Printf("[INFO] Server start? port: %s", addr)
+
+	http.HandleFunc("/hello", s.helloHandler)
+	http.HandleFunc("/", s.defaultHandler)
+
+	return http.ListenAndServe(addr, nil)
+}
+
+func (s *Server) Stop() {
+	// Add your server shutdown logic here
+}
